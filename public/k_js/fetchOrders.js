e6ee9f32ef717056ca3d265ebf4c11cb756d17e7
daba-ITsoldier <kenta0821n@gmail.com>
2025-06-30 18:53:32 +0900

    Add files via upload

--- public/k_js/fetchOrders.js
+++ public/k_js/fetchOrders.js
@@ -2,12 +2,15 @@ function fetchOrders() {
     fetch("/api/orders")
         .then(res => res.json())
         .then(data => {
-        const list = document.getElementById("order-list");
-        list.innerHTML = "";
-        data.forEach(order => {
-            const li = document.createElement("li");
-            li.textContent = `テーブル${order.table}：${order.item} ×${order.quantity}`;
-            list.appendChild(li);
+
+            console.log("受信データ：", data);
+
+            const list = document.getElementById("order-list");
+            list.innerHTML = "";
+            data.forEach(order => {
+                const li = document.createElement("li");
+                li.textContent = `テーブル${order.table}：${order.item} ×${order.quantity}`;
+                list.appendChild(li);
         });
     });
 }
